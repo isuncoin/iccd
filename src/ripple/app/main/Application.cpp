XRPLF/rippled

Transaction queue and fee escalation (RIPD-598):
The first few transactions are added to the open ledger at
the base fee (ie. 10 drops).  Once enough transactions are
added, the required fee will jump dramatically. If additional
transactions are added, the fee will grow exponentially.

Transactions that don't have a high enough fee to be applied to
the ledger are added to the queue in order from highest fee to
lowest. Whenever a new ledger is accepted as validated, transactions
are first applied from the queue to the open ledger in fee order
until either all transactions are applied or the fee again jumps
too high for the remaining transactions.

Current implementation is restricted to one transaction in the
queue per account. Some groundwork has been laid to expand in
the future.

Note that this fee logic escalates independently of the load-based
fee logic (ie. LoadFeeTrack). Submitted transactions must meet
the load fee to be considered for the queue, and must meet both
fees to be put into open ledger.

src/ripple/app/main/Application.cpp
@@ -44,6 +44,7 @@
 #include <ripple/app/misc/UniqueNodeList.h>
 #include <ripple/app/tx/InboundTransactions.h>
 #include <ripple/app/tx/TransactionMaster.h>
+#include <ripple/app/tx/TxQ.h>
 #include <ripple/app/tx/apply.h>
 #include <ripple/basics/Log.h>
 #include <ripple/basics/ResolverAsio.h>
@@ -326,6 +327,7 @@ public:
     std::unique_ptr <HashRouter> mHashRouter;
     std::unique_ptr <Validations> mValidations;
     std::unique_ptr <LoadManager> m_loadManager;
+    std::unique_ptr <TxQ> txQ_;
     beast::DeadlineTimer m_sweepTimer;
     beast::DeadlineTimer m_entropyTimer;
 
@@ -459,6 +461,8 @@ public:
         , mValidations (make_Validations (*this))
 
         , m_loadManager (make_LoadManager (*this, *this, logs_->journal("LoadManager")))
+
+        , txQ_(make_TxQ(setup_TxQ(*config_), logs_->journal("TxQ")))
 
         , m_sweepTimer (this)
 
@@ -681,6 +685,12 @@ public:
     Overlay& overlay () override
     {
         return *m_overlay;
+    }
+
+    TxQ& getTxQ() override
+    {
+        assert(txQ_.get() != nullptr);
+        return *txQ_;
     }
 
     DatabaseCon& getTxnDB () override

src/ripple/rpc/handlers/Fee1.cpp
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 /*
     This file is part of rippled: https://github.com/ripple/rippled
-    Copyright (c) 2012, 2013 Ripple Labs Inc.
+    Copyright (c) 2012-2015 Ripple Labs Inc.
 
     Permission to use, copy, modify, and/or distribute this software for any
     purpose  with  or without fee is hereby granted, provided that the above
@@ -18,19 +18,24 @@
 //==============================================================================
 
 #include <BeastConfig.h>
+#include <ripple/app/tx/TxQ.h>
+#include <ripple/app/ledger/LedgerMaster.h>
+#include <ripple/rpc/Context.h>
+#include <ripple/protocol/ErrorCodes.h>
+#include <ripple/protocol/Feature.h>
 
-#include <ripple/app/tests/AccountTxPaging.test.cpp>
-#include <ripple/app/tests/AmendmentTable.test.cpp>
-#include <ripple/app/tests/CrossingLimits_test.cpp>
-#include <ripple/app/tests/DeliverMin.test.cpp>
-#include <ripple/app/tests/HashRouter_test.cpp>
-#include <ripple/app/tests/MultiSign.test.cpp>
-#include <ripple/app/tests/OfferStream.test.cpp>
-#include <ripple/app/tests/Offer.test.cpp>
-#include <ripple/app/tests/Path_test.cpp>
-#include <ripple/app/tests/Regression_test.cpp>
-#include <ripple/app/tests/SusPay_test.cpp>
-#include <ripple/app/tests/SetAuth_test.cpp>
-#include <ripple/app/tests/OversizeMeta_test.cpp>
-#include <ripple/app/tests/Taker.test.cpp>
-#include <ripple/app/tests/TxQ_test.cpp>
+namespace ripple
+{
+    Json::Value doFee(RPC::Context& context)
+    {
+        // Bail if fee escalation is not enabled.
+        if (!context.app.getLedgerMaster().getValidatedRules().
+            enabled(featureFeeEscalation, context.app.config().features))
+        {
+            RPC::inject_error(rpcNOT_ENABLED, context.params);
+            return context.params;
+        }
+
+        return context.app.getTxQ().doRPC(context.app);
+    }
+} // ripple

src/ripple/unity/app_tx.cpp
@@ -42,3 +42,4 @@
 #include <ripple/app/tx/impl/ApplyContext.cpp>
 #include <ripple/app/tx/impl/TransactionAcquire.cpp>
 #include <ripple/app/tx/impl/Transactor.cpp>
+#include <ripple/app/tx/impl/TxQImpl.cpp>

isuncoin/iccd

src/ripple/app/main/Application.cpp
@@ -38,6 +38,7 @@
 #include <ripple/app/misc/HashRouter.h>
 #include <ripple/app/misc/NetworkOPs.h>
 #include <ripple/app/misc/SHAMapStore.h>
+#include <ripple/app/misc/TxQ.h>
 #include <ripple/app/misc/Validations.h>
 #include <ripple/app/paths/Pathfinder.h>
 #include <ripple/app/paths/PathRequests.h>
@@ -326,6 +327,7 @@ public:
     std::unique_ptr <HashRouter> mHashRouter;
     std::unique_ptr <Validations> mValidations;
     std::unique_ptr <LoadManager> m_loadManager;
+    std::unique_ptr <TxQ> txQ_;
     beast::DeadlineTimer m_sweepTimer;
     beast::DeadlineTimer m_entropyTimer;
 
@@ -459,6 +461,8 @@ public:
         , mValidations (make_Validations (*this))
 
         , m_loadManager (make_LoadManager (*this, *this, logs_->journal("LoadManager")))
+
+        , txQ_(make_TxQ(setup_TxQ(*config_), logs_->journal("TxQ")))
 
         , m_sweepTimer (this)
 
@@ -681,6 +685,12 @@ public:
     Overlay& overlay () override
     {
         return *m_overlay;
+    }
+
+    TxQ& getTxQ() override
+    {
+        assert(txQ_.get() != nullptr);
+        return *txQ_;
     }
 
     DatabaseCon& getTxnDB () override

src/ripple/rpc/handlers/Fee1.cpp
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
 /*
     This file is part of rippled: https://github.com/ripple/rippled
-    Copyright (c) 2012, 2013 Ripple Labs Inc.
+    Copyright (c) 2012-2015 Ripple Labs Inc.
 
     Permission to use, copy, modify, and/or distribute this software for any
     purpose  with  or without fee is hereby granted, provided that the above
@@ -18,19 +18,24 @@
 //==============================================================================
 
 #include <BeastConfig.h>
+#include <ripple/app/misc/TxQ.h>
+#include <ripple/app/ledger/LedgerMaster.h>
+#include <ripple/rpc/Context.h>
+#include <ripple/protocol/ErrorCodes.h>
+#include <ripple/protocol/Feature.h>
 
-#include <ripple/app/tests/AccountTxPaging.test.cpp>
-#include <ripple/app/tests/AmendmentTable.test.cpp>
-#include <ripple/app/tests/CrossingLimits_test.cpp>
-#include <ripple/app/tests/DeliverMin.test.cpp>
-#include <ripple/app/tests/HashRouter_test.cpp>
-#include <ripple/app/tests/MultiSign.test.cpp>
-#include <ripple/app/tests/OfferStream.test.cpp>
-#include <ripple/app/tests/Offer.test.cpp>
-#include <ripple/app/tests/Path_test.cpp>
-#include <ripple/app/tests/Regression_test.cpp>
-#include <ripple/app/tests/SusPay_test.cpp>
-#include <ripple/app/tests/SetAuth_test.cpp>
-#include <ripple/app/tests/OversizeMeta_test.cpp>
-#include <ripple/app/tests/Taker.test.cpp>
-#include <ripple/app/tests/TxQ_test.cpp>
+namespace ripple
+{
+    Json::Value doFee(RPC::Context& context)
+    {
+        // Bail if fee escalation is not enabled.
+        if (!context.app.getLedgerMaster().getValidatedRules().
+            enabled(featureFeeEscalation, context.app.config().features))
+        {
+            RPC::inject_error(rpcNOT_ENABLED, context.params);
+            return context.params;
+        }
+
+        return context.app.getTxQ().doRPC(context.app);
+    }
+} // ripple

src/ripple/unity/app_tx.cpp
@@ -20,6 +20,7 @@
 #include <BeastConfig.h>
 
 #include <ripple/app/tx/impl/apply.cpp>
+#include <ripple/app/tx/impl/applySteps.cpp>
 #include <ripple/app/tx/impl/BookTip.cpp>
 #include <ripple/app/tx/impl/CancelOffer.cpp>
 #include <ripple/app/tx/impl/CancelTicket.cpp>